afcd1e8409d6b4a947d816c31079305e5f4c70d1
NarutoUzumakioftheleaf <deepapandey717@gmail.com>
2024-12-02 16:39:58 +0530

    Added:random number generation from 1 to 6

--- Main.java
+++ Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome to this game!");
-
+        System.out.printf("Hello and welcome to this game! \n");
+        int number=(int)(Math.random()*6)+1;
+        System.out.println("your Dice is rolling...");
+        System.out.println("Hey you get number "+ number);
     }
 }
